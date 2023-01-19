a0f5ad8f951a63c33d751609b886f034ebcdbd88
abhirampjayan <abhiramp08@gmail.com>
2023-01-20 00:49:43 +0530

    grouping added

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,14 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-test('Greet test', () => {
-  render(<Greet />);
-  const element = screen.getByText('Greet');
-  expect(element).toBeInTheDocument();
+describe.only('Greet', () => {
+  test('Greet test', () => {
+    render(<Greet />);
+    const element = screen.getByText('Greet');
+    expect(element).toBeInTheDocument();
+  });
+  test.skip('Greet test 2', () => {
+    render(<Greet />);
+    const element = screen.getByText('Greet');
+    expect(element).toBeInTheDocument();
+  });
+  describe('Nested', () => {
+    test('Greet test with name', () => {
+      render(<Greet name="hello" />);
+      const element = screen.getByText('hello');
+      expect(element).toBeInTheDocument();
+    });
+  });
 });
-
-test('Greet test with name', () => {
-  render(<Greet name="hello" />);
-  const element = screen.getByText('hello');
-  expect(element).toBeInTheDocument();
+describe('Skipped Group', () => {
+  test('Greet test with name', () => {
+    render(<Greet name="hello" />);
+    const element = screen.getByText('hello');
+    expect(element).toBeInTheDocument();
+  });
 });
